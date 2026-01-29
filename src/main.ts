84696b3fe9fe1ba9b2eac8d7081eed2579edf1d3
rmMarioAlberto <rmmarioalberto08@gmail.com>
2026-01-29 10:55:03 -0600

    helmet implement

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,27 @@ import { globalValidationPipe } from './common/pipes/validation.pipe';
 import { corsConfig } from './config/cors.config';
 import { AllExceptionsFilter } from './common/filters/exceptions.filter';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  const isProduction = configService.get<string>('NODE_ENV') === 'production';
+
+  app.use(
+    helmet({
+      contentSecurityPolicy: false, // APIs REST no envían HTML
+      frameguard: { action: 'deny' }, // "X-Frame-Options: DENY"
+      referrerPolicy: { policy: 'no-referrer' }, // Oculta la URL de origen en peticiones salientes
+      hsts: isProduction
+        ? {
+            maxAge: 63072000, // Mantenemos 2 años
+            includeSubDomains: true,
+            preload: true,
+          }
+        : false,
+    }),
+  );
 
   // Inicializar Sentry
   initSentry(configService);
